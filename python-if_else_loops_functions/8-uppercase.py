cb06b9db5f5c63de3909784d84db990ed8c49dff
Lauro <laurolamas@gmail.com>
2023-01-22 15:05:46 -0300

    Added_8

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def islower(c):
+    if ord(c) >= ord('a') and ord(c) <= ord('z'):
+        return True
+    else:
+        return False
+
+
+def uppercase(str):
+    for i in range(0, len(str)):
+        print("{}".format(chr(ord(str[i]) - 32) if islower(str[i])
+              else str[i]), end="")
+
+    print()
