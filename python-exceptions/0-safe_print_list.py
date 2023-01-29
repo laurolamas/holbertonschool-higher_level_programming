0ea7500fad7ef6bbd72f51472c3a135c563d4d4c
Lauro <laurolamas@gmail.com>
2023-01-29 16:31:44 -0300

    Added_0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/pyhton3
+
+
+def safe_print_list(my_list=[], x=0):
+
+    nb = 0
+
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            nb += 1
+        except IndexError:
+            break
+
+    print()
+
+    return nb
