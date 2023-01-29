a42e26096bb77f29e26953d9a8f84bd51c2864e5
Lauro <laurolamas@gmail.com>
2023-01-29 14:32:41 -0300

    Added_8

---
+++ python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def simple_delete(a_dictionary, key=""):
+
+    if a_dictionary.get(key) is None or key == "":
+        return a_dictionary
+
+    else:
+        a_dictionary.pop(key)
+
+    return a_dictionary
