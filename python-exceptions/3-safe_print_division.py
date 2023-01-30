58661364f2e3fcb9df5163d6e1e999e8a495b8f6
Lauro <laurolamas@gmail.com>
2023-01-29 22:03:10 -0300

    Fix_3.1

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -9,7 +9,6 @@ def safe_print_division(a, b):
     finally:
         try:
             print("Inside result: {}".format(c))
-            print("{}".format(c))
             return c
         except (ValueError, ZeroDivisionError, UnboundLocalError):
             print("Inside result: None")
