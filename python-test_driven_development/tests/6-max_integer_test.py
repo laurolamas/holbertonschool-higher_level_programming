839ba33b4249c3649dc8b031de22690bc6273828
Lauro <laurolamas@gmail.com>
2023-02-04 16:46:39 -0300

    Fix_6.1

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -17,6 +17,9 @@ class Max_int_test(unittest.TestCase):
         self.assertAlmostEqual(max_integer([4, 2, 3, 4]), 4)
         self.assertAlmostEqual(max_integer([1, 5, 3, 4]), 5)
         self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([1, -1, 3, 4]), 4)
+        self.assertAlmostEqual(max_integer([-3, -1, -3, -4]), -1)
+        self.assertAlmostEqual(max_integer([1]), 1)
 
     def test_values(self):
         self.assertRaises(TypeError, max_integer, None)
