4062bd2e427147e2b98a77664c8f45dc2daaa136
Lauro <laurolamas@gmail.com>
2023-02-04 15:02:27 -0300

    Added_4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+task 4
+"""
+
+
+def print_square(size):
+
+    if type(size) is not int or size is None:
+        raise TypeError("size must be an integer")
+
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print()
