2ae28a2a7721b417dede9b8cbb9a960daa6e5f41
Lauro <laurolamas@gmail.com>
2023-02-04 13:33:56 -0300

    Added_3

---
+++ python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Task 2
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Say my name"""
+
+    if first_name is None:
+        raise TypeError("first_name must be a string")
+
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+
+    if type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+
+    print("My name is {} {}".format(first_name, last_name))
