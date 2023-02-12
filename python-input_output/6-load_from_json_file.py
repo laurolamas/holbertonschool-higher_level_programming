94e6228fad99a807b812c4a74e90d54c126cc9f8
Lauro <laurolamas@gmail.com>
2023-02-12 12:19:57 -0300

    Added_6

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+""" TAsk 6"""
 
 
 import json
 
 
 def load_from_json_file(filename):
+    """Load"""
     with open(filename, mode="r", encoding="utf-8") as f:
         return json.load(f)
