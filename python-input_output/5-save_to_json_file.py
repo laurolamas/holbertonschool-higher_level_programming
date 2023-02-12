fafd0a6f2f8ac6e1baba6b3baf99d69fc197b806
Lauro <laurolamas@gmail.com>
2023-02-12 12:20:10 -0300

    Fix_5.1

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+"""Task 5"""
 
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
+    """SAve"""
     with open(filename, mode="a", encoding="utf-8") as f:
         json.dump(my_obj, f)
