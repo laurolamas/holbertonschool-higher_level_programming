c5851485396f10ff395671e30ad95f1cc3f13c68
Lauro <laurolamas@gmail.com>
2023-02-18 14:42:46 -0300

    Added_3

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -4,10 +4,42 @@
 from models.base import Base
 
 
+def check_width(width):
+    if type(width) is not int:
+        raise TypeError("width must be an integer")
+    if width <= 0:
+        raise ValueError("width must be > 0")
+
+
+def check_height(height):
+    if type(height) is not int:
+        raise TypeError("height must be an integer")
+    if height <= 0:
+        raise ValueError("height must be > 0")
+
+
+def check_x(x):
+    if type(x) is not int:
+        raise TypeError("x must be an integer")
+    if x < 0:
+        raise ValueError("x must be >= 0")
+
+
+def check_y(y):
+    if type(y) is not int:
+        raise TypeError("y must be an integer")
+    if y < 0:
+        raise ValueError("y must be >= 0")
+
+
 class Rectangle(Base):
     """Rectangle Class"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        check_width(width)
+        check_height(height)
+        check_x(x)
+        check_y(y)
 
         Base.__init__(self, id)
         self.__width = width
@@ -21,6 +53,7 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
+        check_width(value)
         self.__width = value
 
     @property
@@ -29,6 +62,7 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
+        check_height(value)
         self.__height = value
 
     @property
@@ -37,6 +71,7 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
+        check_x(value)
         self.__x = value
 
     @property
@@ -45,4 +80,5 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
+        check_y(value)
         self.__y = value
