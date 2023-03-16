c05cd8a4d449e73c29fc8fb1439e79edc7f2c05a
Lauro <laurolamas@gmail.com>
2023-03-16 14:27:32 -0300

    Added_2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Task 0 """
+
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3])
+
+    cur = db.cursor()
+
+    cur.execute(f"SELECT * FROM states WHERE name LIKE '{sys.argv[4]}'")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
