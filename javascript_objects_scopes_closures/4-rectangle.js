ac47a8df3be277df7875abac24ee9fbd1232ec92
Lauro <laurolamas@gmail.com>
2023-04-15 13:34:16 -0300

    Added_4

---
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,35 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (!(w <= 0 || h <= 0 || isNaN(w) || isNaN(h))) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let j = 0; j < this.width; j++) {
+        process.stdout.write('X');
+      }
+      if (i !== this.height - 1) {
+        console.log();
+      }
+    }
+    console.log();
+  }
+
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+}
+
+module.exports = Rectangle;
