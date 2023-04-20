73c693b0ef39de649fcc12ff18a590218a3b349e
Lauro <laurolamas@gmail.com>
2023-04-20 11:31:39 -0300

    added_2

---
+++ javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const url = process.argv[2];
+
+const request = require('request');
+
+request(url, function (error, response, body) {
+  if (error) throw error;
+  console.log(`code: ${response.statusCode}`);
+});
